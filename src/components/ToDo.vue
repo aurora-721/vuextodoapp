cd5667546cafa8d21aac1a938752d4b02825970b
aurora-721 <aurora.makovac@gmail.com>
2022-08-08 11:37:47 +0200

    adding todos in store

---
+++ src/components/ToDo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h3>Todos</h3>
+    <div class="todos">
+        <div v-for="todo in allTodos" :key="todo.id" class="todo">
+            {{todo.title}}
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'ToDo',
+  props: {
+    msg: String
+  },
+  computed: mapGetters(['allTodos'])
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
